aa0c668e7c90af3cf6a06f0a75b5c6e0702b7a45
Marion Lalonde <lalondemarion.perso@gmail.com>
2025-04-10 19:01:28 +0200

    chore(global): Fix backgrounds importation

---
+++ src/renderer/src/assets/backgrounds/index.ts
@@ -0,0 +1,23 @@
+import bg1 from './bg-46B7A7.png'
+import bg2 from './bg-87DFE2.png'
+import bg3 from './bg-468FB7.png'
+import bg4 from './bg-C995CD.png'
+import bg5 from './bg-F2ABE7.png'
+import bg6 from './bg-F8D7A5.png'
+import bg7 from './bg-F68052.png'
+import bg8 from './bg-FE8D8D.png'
+import bg9 from './bg-FEEA6D.png'
+
+export const backgroundsMap: Record<string, string> = {
+  'bg-46B7A7.png': bg1,
+  'bg-87DFE2.png': bg2,
+  'bg-468FB7.png': bg3,
+  'bg-C995CD.png': bg4,
+  'bg-F2ABE7.png': bg5,
+  'bg-F8D7A5.png': bg6,
+  'bg-F68052.png': bg7,
+  'bg-FE8D8D.png': bg8,
+  'bg-FEEA6D.png': bg9
+}
+
+export const backgroundKeys = Object.keys(backgroundsMap)
